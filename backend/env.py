ae4f9cf78591005d5b5927e453f7e9837cb59a69
Alena Baklanskaya <73777191+Alena18@users.noreply.github.com>
2024-12-02 20:00:18 +0000

    Delete and rewrite

--- backend/env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ["DATABASE_URL"]  = "postgres://phndpuls:zoSq1tGOwD2XZlHna_TGWPeQm6I40Urn@kandula.db.elephantsql.com/phndpuls"
-os.environ["SECRET_KEY"] = "django-insecure-q6e5y@*ta0=^g*v)r=2bs19qd1k*u(whp9-!m41=)^crk+#g*j"
--- treereminder/src/components/LoadingIndicator.jsx
+++ treereminder/src/components/LoadingIndicator.jsx
@@ -1,9 +1,9 @@
 const LoadingIndicator = () => {
-  return (
-    <div className="loading-container">
-      <div className="loader"></div>
-    </div>
-  );
-};
-
-export default LoadingIndicator;
+    return (
+      <div className="loading-container">
+        <div className="loader"></div>
+      </div>
+    );
+  };
+  
+  export default LoadingIndicator;
